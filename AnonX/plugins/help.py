ca6d31806dd256b5cd093484ac9a76b93edb2be3
sumit <118124900+sumit-yu-bot@users.noreply.github.com>
2023-05-17 03:49:35 +0530

    Update help.py

--- AnonX/plugins/help.py
+++ AnonX/plugins/help.py
@@ -59,7 +59,7 @@ async def helper_private(
         language = await get_lang(chat_id)
         _ = get_string(language)
         keyboard = help_pannel(_)
-        await update.reply_sticker("CAACAgUAAxkBAAIjVmKPYTFByKZlCo9d8mUv8QVAJEw7AAL9BQACiy14VGoQxOCDfE1KJAQ")
+        await update.reply_sticker("CAACAgUAAx0CbYIhvgACGn5kZAFAtFWz0ChBFFNTKgFaD8veQQACrgUAAlBeqFaTvxW9CYa2vB4E")
         await update.reply_photo(
             photo=config.START_IMG_URL,
             caption=_["help_1"].format(config.SUPPORT_HEHE), reply_markup=keyboard)
