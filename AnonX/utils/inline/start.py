4fe80481872e5d81c8f76195f501781eaccc1446
sumit <118124900+sumit-yu-bot@users.noreply.github.com>
2023-05-19 21:00:32 +0530

    Update start.py

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -9,17 +9,17 @@ def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text="✭ᴀᴅᴅ ᴇʟꜱᴇ ʏᴏᴜ ɢᴀʏ✭",
+                text="● ᴀᴅᴅ ᴇʟꜱᴇ ʏᴏᴜ ɢᴀʏ ●",
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
         ],
         [
             InlineKeyboardButton(
-                text="✭ʜᴇʟᴩ✭",
+                text="● ʜᴇʟᴩ ●",
                 callback_data="settings_back_helper",
             ),
             InlineKeyboardButton(
-                text="✭sᴇᴛᴛɪɴɢs✭", callback_data="settings_helper"
+                text="● sᴇᴛᴛɪɴɢs ●", callback_data="settings_helper"
             ),
         ],
      ]
@@ -30,31 +30,31 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text="♡★ᴀᴅᴅ ᴇʟꜱᴇ ʏᴏᴜ ɢᴀʏ★♡",
+                text="● ᴀᴅᴅ ᴇʟꜱᴇ ʏᴏᴜ ɢᴀʏ ●",
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
         ],
         [
             InlineKeyboardButton(
-                text="♡★ʜᴇʟᴘ ᴀɴᴅ ᴄᴏᴍᴍᴀɴᴅꜱ★♡", callback_data="settings_back_helper"
+                text="● ʜᴇʟᴘ ᴀɴᴅ ᴄᴏᴍᴍᴀɴᴅꜱ ●", callback_data="settings_back_helper"
             )
         ],
         [
             InlineKeyboardButton(
-                text="♡sᴜᴩᴩᴏʀᴛ♡", url=config.SUPPORT_GROUP
+                text="● sᴜᴩᴩᴏʀᴛ ●", url=config.SUPPORT_GROUP
             ),
             InlineKeyboardButton(
-                text="★[ᴅᴇᴠᴇʟᴏᴘᴇʀ]★", user_id=OWNER
+                text="● ᴅᴇᴠᴇʟᴏᴘᴇʀ ●", user_id=OWNER
             )
         ],
          [
             InlineKeyboardButton(
-                text="♡ᴜᴘᴅᴀᴛᴇꜱ♡", url=f"https://t.me/ab_sumit",
+                text="● ᴜᴘᴅᴀᴛᴇꜱ ●", url=f"https://t.me/ab_sumit",
             )
         ],
         [
             InlineKeyboardButton(
-                text="♡[ʀᴇᴘᴏ]♡", url=f"https://t.me/ab_sumit",
+                text="● ʀᴇᴘᴏ ●", url=f"https://t.me/ab_sumit",
             )
         ],
      ]
